a1e6201308150d8b5d063361c63b2cf58fc646ef
Giannos Chatziagapis <giannos.chatziagapis@gmail.com>
2024-12-05 17:46:58 +0200

    Use parser in main

--- src/horn.cr
+++ src/horn.cr
@@ -1,114 +1,82 @@
-require "./horn/expressions/*"
-require "./horn/types/*"
-require "./horn/typed_expression"
+require "option_parser"
+
+require "./horn/parser/parser"
 require "./horn/top_down"
 
 module Horn
-  include Expressions
+  VERSION = "0.1.0"
+  filename = nil
+  verbose = false
 
-  # e = [{"1", "2"}, {"2", "3"}, {"1", "4"}]
-  # e = [{"1", "2"}, {"2", "3"}, {"3", "1"}]
-  e = [{"1", "2"}, {"1", "3"}, {"2", "4"}, {"3", "9"}, {"4", "5"}, {"4", "7"}, {"5", "3"}, {"5", "7"}, {"6", "7"}, {"6", "2"}, {"7", "8"}, {"7", "9"}, {"8", "6"}]
-  # e = [{"Z", "W1"}, {"U1", "A"}, {"Q1", "J"}, {"U1", "B1"}, {"B", "T1"}, {"X1", "X"}, {"N1", "Y"}, {"Q1", "F"}, {"B", "K"}, {"G1", "A"}, {"Q1", "I1"}, {"A1", "C1"}, {"S1", "V1"}, {"J", "E1"}, {"W", "D"}, {"F1", "A"}, {"M", "J1"}, {"G1", "N"}, {"J1", "H1"}, {"X1", "T"}, {"E", "Y"}, {"F1", "D"}, {"F", "U"}, {"S1", "N"}, {"T1", "B1"}, {"J1", "D1"}, {"Z", "V1"}, {"Q1", "U"}, {"K", "P"}, {"W", "I1"}, {"B", "A1"}, {"P1", "I"}, {"P1", "X1"}, {"D", "Q"}, {"W1", "L1"}, {"R", "B"}, {"U1", "M1"}, {"P", "M1"}, {"A", "Q"}, {"X1", "Q1"}, {"O", "I"}, {"M", "Y"}, {"D", "G1"}, {"B1", "O1"}, {"R1", "F1"}, {"B", "A"}, {"E", "P"}, {"A1", "V"}, {"G1", "U"}, {"L1", "C1"}, {"S1", "D"}, {"C", "M"}, {"M1", "W"}, {"R1", "N"}, {"X1", "Q"}, {"F", "R"}, {"D1", "Y"}, {"Z", "Y"}, {"I", "P1"}, {"X1", "W"}, {"C", "C1"}, {"F", "P1"}, {"V1", "H1"}, {"I1", "P"}, {"K", "E"}, {"C", "E"}, {"C1", "A"}, {"N1", "F1"}, {"I", "W1"}, {"P", "N1"}, {"R1", "W"}, {"X1", "C1"}, {"F", "S1"}, {"O1", "C"}, {"G1", "T1"}, {"C1", "T"}, {"C1", "H"}, {"C1", "E1"}, {"E", "J"}, {"V1", "R"}, {"A", "W"}, {"W1", "I"}, {"K", "U"}, {"C", "R1"}, {"I1", "V1"}, {"K", "T1"}, {"J", "K"}, {"O1", "I1"}, {"Y", "P"}, {"G1", "U1"}, {"S1", "B1"}, {"M", "W1"}, {"R1", "B"}, {"P1", "N1"}, {"E1", "O"}, {"I", "Z"}, {"V1", "U"}, {"M", "N"}, {"M1", "M"}, {"C1", "J"}]
-  v = e.map(&.to_a).flatten.uniq
-  # v << "alone"
+  options_parser = OptionParser.parse do |options_parser|
+    options_parser.banner = "Usage: horn [options]"
 
-  p = {Pred.new("v") => Lambda.new(Var.new("X"), Types::I,
-    Or.from_list(v.map { |x| Eq.new(Var.new("X"), Const.new(x.to_s)) })
-  ).as(Expr)}
+    options_parser.on "-h", "--help", "Show this help" do
+      puts options_parser
+      exit
+    end
 
-  p.merge!({Pred.new("e") => Lambda.new(Var.new("X"), Types::I,
-    Lambda.new(Var.new("Y"), Types::I,
-      Or.from_list(e.map { |x, y| And.new(Eq.new(Var.new("X"), Const.new(x.to_s)), Eq.new(Var.new("Y"), Const.new(y.to_s))) })
-    )).as(Expr)})
+    options_parser.on "--version", "Show version" do
+      puts "HORN v#{VERSION}"
+      exit
+    end
 
-  p.merge!({
-    Pred.new("remove") => Lambda.new(Var.new("V"), Types::Arrow.new(Types::I, Types::O),
-      Lambda.new(Var.new("A"), Types::I,
-        Lambda.new(Var.new("X"), Types::I,
-          And.new(
-            Appl.new(Var.new("V"), Var.new("X")),
-            Not.new(Eq.new(Var.new("X"), Var.new("A")))
-          )
-        ))).as(Expr),
-    Pred.new("remove2") => Lambda.new(Var.new("E"), Types::Arrow.new(Types::I, Types::Arrow.new(Types::I, Types::O)),
-      Lambda.new(Var.new("A"), Types::I,
-        Lambda.new(Var.new("X"), Types::I,
-          Lambda.new(Var.new("Y"), Types::I,
-            And.new(
-              Appl.new(Appl.new(Var.new("E"), Var.new("X")), Var.new("Y")),
-              And.new(
-                Not.new(Eq.new(Var.new("X"), Var.new("A"))),
-                Not.new(Eq.new(Var.new("Y"), Var.new("A")))
-              )
-            )
-          )))).as(Expr),
-    Pred.new("winning") => Lambda.new(Var.new("V"), Types::Arrow.new(Types::I, Types::O),
-      Lambda.new(Var.new("E"), Types::Arrow.new(Types::I, Types::Arrow.new(Types::I, Types::O)),
-        Lambda.new(Var.new("X"), Types::I,
-          Exists.new(Var.new("Y"), Types::I,
-            And.from_list([
-              Appl.from_list([Var.new("E"), Var.new("X"), Var.new("Y")]),
-              Not.new(Eq.new(Var.new("X"), Var.new("Y"))),
-              Not.new(Appl.from_list([
-                Pred.new("winning"),
-                Appl.from_list([Pred.new("remove"), Var.new("V"), Var.new("X")]),
-                Appl.from_list([Pred.new("remove2"), Var.new("E"), Var.new("X")]),
-                Var.new("Y"),
-              ])),
-            ])
-          )))).as(Expr),
-    Pred.new("transitive") => Lambda.new(Var.new("R"), Types::Arrow.new(Types::I, Types::Arrow.new(Types::I, Types::O)),
-      Lambda.new(Var.new("X"), Types::I,
-        Lambda.new(Var.new("Y"), Types::I,
-          Or.new(
-            Appl.new(Appl.new(Var.new("R"), Var.new("X")), Var.new("Y")),
-            Exists.new(Var.new("Z"), Types::I,
-              And.new(
-                Appl.new(Appl.new(Var.new("R"), Var.new("X")), Var.new("Z")),
-                Appl.from_list([
-                  Pred.new("transitive"),
-                  Var.new("R"),
-                  Var.new("Z"),
-                  Var.new("Y"),
-                ])
-              )
-            )
-          )
-        ))).as(Expr),
-    Pred.new("disconnected") => Lambda.new(Var.new("V"), Types::Arrow.new(Types::I, Types::O),
-      Lambda.new(Var.new("E"), Types::Arrow.new(Types::I, Types::Arrow.new(Types::I, Types::O)),
-        Lambda.new(Var.new("X"), Types::I,
-          Exists.new(Var.new("Y"), Types::I,
-            And.new(
-              Not.new(Eq.new(Var.new("X"), Var.new("Y"))),
-              Not.new(Appl.from_list([
-                Pred.new("transitive"), Var.new("E"), Var.new("X"), Var.new("Y"),
-              ]))
-            )
-          )))).as(Expr),
-  })
+    options_parser.on "-f FILE", "--file=FILE", "HORN file" do |f|
+      filename = f
+    end
 
-  objects = v.map { |x| TypedExpr.new(Const.new(x.to_s), Types::I) }
-  objects << TypedExpr.new(Pred.new("e"), Types::Arrow.new(Types::I, Types::Arrow.new(Types::I, Types::O)))
-  objects << TypedExpr.new(Pred.new("v"), Types::Arrow.new(Types::I, Types::O))
-  objects << TypedExpr.new(Pred.new("transitive"), Types::Arrow.new(Types::Arrow.new(Types::O, Types::O), Types::Arrow.new(Types::I, Types::O)))
+    options_parser.on "-v", "--verbose", "Verbose mode" do
+      verbose = true
+    end
 
-  strategy = TopDown.new(p, objects)
+    options_parser.invalid_option do |option|
+      puts "Invalid option: #{option}"
+      puts options_parser
+      exit(1)
+    end
+  end
 
-  puts strategy.eval(Appl.from_list([
-    Pred.new("winning"),
-    Pred.new("v"),
-    Pred.new("e"),
-    Const.new(v.first.to_s),
-  ]))
+  if filename.nil?
+    puts options_parser
+    exit(1)
+  end
 
-  # puts strategy.eval(Appl.from_list([
-  #   Pred.new("disconnected"),
-  #   Pred.new("v"),
-  #   Pred.new("e"),
-  #   Const.new(v.first.to_s),
-  # ]))
+  parser = Parser.new(filename.not_nil!)
 
-  # puts strategy.visualize
+  begin
+    parser.parse
+  rescue e
+    puts e.message
+    exit(2)
+  end
+
+  if verbose
+    puts "Program:"
+    puts parser.program.to_pretty_json
+    puts "Const collection:"
+    puts parser.const_collection.to_pretty_json
+    puts "Queries:"
+    puts parser.queries.to_json
+    puts "Run:"
+  end
+
+  strategy = TopDown.new(parser.program, parser.const_collection)
+
+  begin
+    parser.queries.each do |query|
+      puts "#{query} => #{strategy.eval(query)}"
+    end
+  rescue e
+    puts e.message
+    if verbose
+      puts "Evaluation graph:"
+      puts strategy.visualize
+    end
+    exit(2)
+  end
+
+  if verbose
+    puts "Evaluation graph:"
+    puts strategy.visualize
+  end
 end
