448e49a6d04579efddbfb84abcd2d565779609db
Giannos Chatziagapis <giannos.chatziagapis@gmail.com>
2024-12-10 21:10:57 +0200

    Introduce forall

---
+++ src/horn/expressions/forall.cr
@@ -0,0 +1,22 @@
+require "./expr"
+
+module Horn
+  module Expressions
+    class Forall < Expr
+      property var : Var
+      property var_type : Type
+      property expr : Expr
+
+      def initialize(@var : Var, @var_type : Type, @expr : Expr)
+      end
+
+      def to_s(io)
+        io << "(∀#{var}:#{var_type}.#{expr})"
+      end
+
+      def inspect(io)
+        io << "(∀#{var}:#{var_type}.#{expr})"
+      end
+    end
+  end
+end
