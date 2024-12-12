5cb6848270cb6abd9cbde84c92328a09589ccc02
Giannos Chatziagapis <giannos.chatziagapis@gmail.com>
2024-12-12 14:45:02 +0200

    Update README

--- spec/unit/parser_spec.cr
+++ spec/unit/parser_spec.cr
@@ -11,5 +11,16 @@ module Horn
       parser.const_collection.size.should eq(4)
       parser.queries.size.should eq(1)
     end
+
+    it "parses free variables" do
+      parser = Parser.new("#{__DIR__}/../fixtures/equals.horn")
+      parser.parse
+      parser.program.has_key?(Expressions::Const.new("equals")).should be_true
+      parser.const_collection.has_key?(Expressions::Const.new("p")).should be_true
+      parser.queries.size.should eq(1)
+      parser.queries[0].should eq(
+        Expressions::Appl.from_list([Expressions::Const.new("equals"), Expressions::Const.new("p"), Expressions::Var.new("Q_")])
+      )
+    end
   end
 end
